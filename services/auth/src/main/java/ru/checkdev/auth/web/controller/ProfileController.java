eecb9c0c61beb55abbfcb93900a721a68b520d3d
KatyaBahai <katyxa-two@yandex.ru>
2025-10-17 16:00:48 +0500

    4. Микросервисные системы [#338434]

--- services/auth/src/main/java/ru/checkdev/auth/web/controller/ProfileController.java
+++ services/auth/src/main/java/ru/checkdev/auth/web/controller/ProfileController.java
@@ -1,5 +1,7 @@
 package ru.checkdev.auth.web.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,7 @@ import java.util.List;
  * @author Dmitry Stepanov
  * @version 22.09.2023T23:49
  */
+@Tag(name = "ProfileController", description = "Profile REST API")
 @RestController
 @RequestMapping("/profiles")
 @Slf4j
@@ -32,6 +35,10 @@ public class ProfileController {
      * @param id ID ProfileDTO
      * @return ResponseEntity
      */
+    @Operation(
+            summary = "Get Profile by user id",
+            description = ""
+    )
     @GetMapping("/{id}")
     public ResponseEntity<ProfileDTO> getProfileById(@PathVariable int id) {
         var profileDTO = profileService.findProfileByID(id);
@@ -45,6 +52,10 @@ public class ProfileController {
      *
      * @return ResponseEntity
      */
+    @Operation(
+            summary = "Get all profiles",
+            description = "get all profiles iin order by creation date"
+    )
     @GetMapping("/")
     public ResponseEntity<List<ProfileDTO>> getAllProfilesOrderByCreateDesc() {
         var profiles = profileService.findProfilesOrderByCreatedDesc();
@@ -53,6 +64,10 @@ public class ProfileController {
                 profiles.isEmpty() ? HttpStatus.NO_CONTENT : HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Get all profiles by user ids",
+            description = "get all profiles associated with user ids provided by the incoming parameter"
+    )
     @PostMapping("/by-ids")
     public ResponseEntity<List<ProfileDTO>> getProfilesByUserIds(@RequestBody List<Integer> userIds) {
         var profiles = profileService.findProfilesByUserIds(userIds);
